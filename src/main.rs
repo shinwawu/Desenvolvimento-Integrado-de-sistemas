d687a1ba32eae7d9fadbaf4612fb8dbf38204d99
Washington Wu <washingtonying@hotmail.com>
2026-04-13 15:37:08 -0300

    construcao do server em rust

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::sync::{Arc, LazyLock, Mutex};
-
+use polars::prelude::*;
 // estrutura para representar um modelo
 struct Model {
     s: i32,
@@ -21,7 +21,7 @@ static MODELS_H: LazyLock<HashMap<String, Mutex<Model>>> = LazyLock::new(|| {
             s: 50816,
             n: 3600,
             shape: (60, 60),
-            path: String::from("H-1.npz"),
+            path: String::from("../data/H-1.npz"),
             matrix: None,
             prioridade: String::from("baixa"),
             in_use: false,
@@ -34,7 +34,7 @@ static MODELS_H: LazyLock<HashMap<String, Mutex<Model>>> = LazyLock::new(|| {
             s: 27904,
             n: 900,
             shape: (30, 30),
-            path: String::from("H-2.npz"),
+            path: String::from("../data/H-2.npz"),
             matrix: None,
             prioridade: String::from("alta"),
             in_use: false,
@@ -50,10 +50,25 @@ fn verificar_matrix(model_id: &str) -> Option<Arc<Vec<usize>>> {
     // trava o mutex para acessar os dados do modelo
     let model = model_mutex.lock().unwrap();
 
-    // se a matriz existir, pegamos o ponteiro e compartilhamos
-    model.matrix.clone() 
-}
-
+    // verifica se a matriz já foi carregada, se sim, retorna a matriz 
+    if model.matrix.is_some() {
+        
+        return model.matrix.clone();
+    }
+    else {
+        model.matrix = polars::prelude::LazyFrame::scan_npz(&model.path)
+            .ok()?
+            .collect()
+            .ok()?
+            .column("matrix")?
+            .utf8()?
+            .get(0)
+            .and_then(|s| s.parse::<Vec<usize>>().ok())
+            .map(Arc::new);
+    };
+    return model.matrix.clone();
+    
+};
 fn main() {
     if let Some(matriz_compartilhada) = verificar_matrix("60x60") {
         println!("matriz obtida de outro modelo, tamanho: {}", matriz_compartilhada.len());
